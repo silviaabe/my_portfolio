885dbf15469d9595d9a263a6f8a077accb5cfd1f
Silvia Abe <silviaabebuzatto@gmail.com>
2025-02-19 07:28:12 -0300

    finalizando o portfolio

--- script.js
+++ script.js
@@ -48,3 +48,26 @@ document.addEventListener("scroll", () => {
       section.style.opacity = visibleRatio;
   });
 });
+
+window.addEventListener('scroll', () => {
+  const sections = document.querySelectorAll('section');
+  const navLinks = document.querySelectorAll('.nav-link');
+
+  let current = '';
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+
+    if (window.scrollY >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute('id');
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove('active');
+    if (link.getAttribute('href').includes(current)) {
+      link.classList.add('active');
+    }
+  });
+});
